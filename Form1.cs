f5e76f70b9b00534c3fed2a59b37d3059d17b641
bitstobrowser <bitstobrowser@gmail.com>
2017-10-23 00:12:10 -0700

    Now cycle through top n pages

--- Form1.cs
+++ Form1.cs
@@ -44,38 +44,49 @@ namespace NhlDownload
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String eventsUrl = "https://hockey.fantasysports.yahoo.com/hockey/2561/players?&sort=AR&sdir=1&status=ALL&pos=C&stat1=S_S_2017&jsenabled=1";
-            HtmlAgilityPack.HtmlWeb getHtmlWeb = new HtmlAgilityPack.HtmlWeb();
-            HtmlAgilityPack.HtmlDocument document = getHtmlWeb.Load(eventsUrl);
-
-            // Page is loaded, go through the stats for each player
-            var players = document.DocumentNode.SelectNodes("//div[@id='players-table']//div[@class='players']//table//tbody//tr");
-            Debug.WriteLine("Players found =" + players.Count());
             int playerCounter = 0;
-            foreach (HtmlAgilityPack.HtmlNode player in players)
+            for (int pages=0; pages < 26; pages++)
             {
-                var items = player.Descendants("td");
-                int rowTypeCounter = 0;
-                foreach (HtmlAgilityPack.HtmlNode item in items)
+                String baseUrl = "https://hockey.fantasysports.yahoo.com/hockey/2561/players?status=ALL&pos=P&cut_type=33&stat1=S_S_2017&myteam=0&sort=AR&sdir=1";
+                String pageUrl;
+                if (pages ==0)
                 {
-                    switch ((RowType)rowTypeCounter)
-                    {
-                        case RowType.WatchList:
-                            break;
-
-                        case RowType.Player:
-                            string playerName = item.SelectSingleNode(".//a[@class='Nowrap name F-link']").InnerText;
-                            string teamPosition = item.SelectSingleNode(".//span[@class='Fz-xxs']").InnerText;
-                            string[] tm = teamPosition.Split('-');
-                            string team = tm[0].Trim();
-                            string position = tm[1].Replace(",","-").Trim();
-                            Debug.Write(playerName + "," + team + "," + position);
-                            break;
-                    }
-                    rowTypeCounter++;
+                    pageUrl = baseUrl; 
+                } else
+                {
+                    pageUrl = baseUrl + "&count=" + (pages * 25);
                 }
-                playerCounter++;
-                Debug.WriteLine("");
+
+                HtmlAgilityPack.HtmlWeb getHtmlWeb = new HtmlAgilityPack.HtmlWeb();
+                HtmlAgilityPack.HtmlDocument document = getHtmlWeb.Load(pageUrl);
+
+                // Page is loaded, go through the stats for each player
+                var players = document.DocumentNode.SelectNodes("//div[@id='players-table']//div[@class='players']//table//tbody//tr");
+                Debug.WriteLine("Players found =" + players.Count());
+                foreach (HtmlAgilityPack.HtmlNode player in players)
+                {
+                    var items = player.Descendants("td");
+                    int rowTypeCounter = 0;
+                    foreach (HtmlAgilityPack.HtmlNode item in items)
+                    {
+                        switch ((RowType)rowTypeCounter)
+                        {
+                            case RowType.WatchList:
+                                break;
+
+                            case RowType.Player:
+                                string playerName = item.SelectSingleNode(".//a[@class='Nowrap name F-link']").InnerText;
+                                string teamPosition = item.SelectSingleNode(".//span[@class='Fz-xxs']").InnerText;
+                                string[] tm = teamPosition.Split('-');
+                                string team = tm[0].Trim();
+                                string position = tm[1].Replace(",", "-").Trim();
+                                Debug.Write((pages*25) + ": " + playerName + "," + team + "," + position);
+                                break;
+                        }
+                        rowTypeCounter++;
+                    }
+                    Debug.WriteLine("");
+                }
             }
         }
     }
