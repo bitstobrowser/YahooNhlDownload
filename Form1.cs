fc83c520d9949337d51b07811c6c379094eecb94
bitstobrowser <bitstobrowser@gmail.com>
2017-10-23 21:48:52 -0700

    Parsed 600 players to CSV in console debug window

--- Form1.cs
+++ Form1.cs
@@ -18,11 +18,12 @@ namespace NhlDownload
             InitializeComponent();
         }
 
-        enum RowType
+        enum PlayerTableColumnInfo
         {
             WatchList,
             Player,
-            AddDrop,
+            AddDropTrade,
+            Opponent,
             Owner,
             GamesPlayed,
             PreSeasonRanking,
@@ -44,8 +45,7 @@ namespace NhlDownload
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int playerCounter = 0;
-            for (int pages=0; pages < 26; pages++)
+            for (int pages=0; pages < 26; pages++) //26
             {
                 String baseUrl = "https://hockey.fantasysports.yahoo.com/hockey/2561/players?status=ALL&pos=P&cut_type=33&stat1=S_S_2017&myteam=0&sort=AR&sdir=1";
                 String pageUrl;
@@ -62,28 +62,57 @@ namespace NhlDownload
 
                 // Page is loaded, go through the stats for each player
                 var players = document.DocumentNode.SelectNodes("//div[@id='players-table']//div[@class='players']//table//tbody//tr");
-                Debug.WriteLine("Players found =" + players.Count());
+                //Debug.WriteLine("Players found =" + players.Count());
                 foreach (HtmlAgilityPack.HtmlNode player in players)
                 {
                     var items = player.Descendants("td");
-                    int rowTypeCounter = 0;
+                    int playerTableColumnCounter = 0;
                     foreach (HtmlAgilityPack.HtmlNode item in items)
                     {
-                        switch ((RowType)rowTypeCounter)
+                        switch ((PlayerTableColumnInfo)playerTableColumnCounter)
                         {
-                            case RowType.WatchList:
+                            case PlayerTableColumnInfo.WatchList:
                                 break;
 
-                            case RowType.Player:
+                            case PlayerTableColumnInfo.Player:
                                 string playerName = item.SelectSingleNode(".//a[@class='Nowrap name F-link']").InnerText;
                                 string teamPosition = item.SelectSingleNode(".//span[@class='Fz-xxs']").InnerText;
                                 string[] tm = teamPosition.Split('-');
                                 string team = tm[0].Trim();
                                 string position = tm[1].Replace(",", "-").Trim();
-                                Debug.Write((pages*25) + ": " + playerName + "," + team + "," + position);
+                                Debug.Write(playerName + "," + team + "," + position);
                                 break;
+
+                            //case PlayerTableColumnInfo.AddDropTrade:
+                            //    string adtRaw = item.SelectSingleNode(".//a").Attributes["title"].Value;
+                            //    string[] adt = adtRaw.Split(' ');
+                            //    Debug.Write("," + adt[0].Trim());
+                            //    break;
+
+                            case PlayerTableColumnInfo.Opponent:
+                            case PlayerTableColumnInfo.Owner:
+                            case PlayerTableColumnInfo.GamesPlayed:
+                            case PlayerTableColumnInfo.PreSeasonRanking:
+                            case PlayerTableColumnInfo.CurrentRanking:
+                            case PlayerTableColumnInfo.OwnedPercentage:
+                            case PlayerTableColumnInfo.TimeOnIce:
+                            case PlayerTableColumnInfo.Goals:
+                            case PlayerTableColumnInfo.Assists:
+                            case PlayerTableColumnInfo.Points:
+                            case PlayerTableColumnInfo.PlusMinus:
+                            case PlayerTableColumnInfo.PenaltyMinutes:
+                            case PlayerTableColumnInfo.PowerPlayPoints:
+                            case PlayerTableColumnInfo.ShortHandedPoints:
+                            case PlayerTableColumnInfo.ShotsOnGoal:
+                            case PlayerTableColumnInfo.FaceoffWins:
+                            case PlayerTableColumnInfo.Hits:
+                            case PlayerTableColumnInfo.Blocks:
+                                string value = item.SelectSingleNode(".//div").InnerText;
+                                Debug.Write("," + value);
+                                break;
+
                         }
-                        rowTypeCounter++;
+                        playerTableColumnCounter++;
                     }
                     Debug.WriteLine("");
                 }
