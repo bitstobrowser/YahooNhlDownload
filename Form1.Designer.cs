1e00cee8acefd31b22ef450fff164c617e577605
bitstobrowser <bitstobrowser@gmail.com>
2017-11-22 21:14:33 -0800

    Added label for status Added icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,34 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
+            this.labelLeagueNumber = new System.Windows.Forms.Label();
+            this.textBoxLeagueNumber = new System.Windows.Forms.TextBox();
+            this.checkBoxPointsLeague = new System.Windows.Forms.CheckBox();
+            this.labelStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(221, 98);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(97, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Build CSV File";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelLeagueNumber
+            // 
+            this.labelLeagueNumber.AutoSize = true;
+            this.labelLeagueNumber.Location = new System.Drawing.Point(13, 13);
+            this.labelLeagueNumber.Name = "labelLeagueNumber";
+            this.labelLeagueNumber.Size = new System.Drawing.Size(83, 13);
+            this.labelLeagueNumber.TabIndex = 1;
+            this.labelLeagueNumber.Text = "League Number";
+            // 
+            // textBoxLeagueNumber
+            // 
+            this.textBoxLeagueNumber.Location = new System.Drawing.Point(102, 10);
+            this.textBoxLeagueNumber.Name = "textBoxLeagueNumber";
+            this.textBoxLeagueNumber.Size = new System.Drawing.Size(126, 20);
+            this.textBoxLeagueNumber.TabIndex = 2;
+            // 
+            // checkBoxPointsLeague
+            // 
+            this.checkBoxPointsLeague.AutoSize = true;
+            this.checkBoxPointsLeague.Checked = true;
+            this.checkBoxPointsLeague.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxPointsLeague.Location = new System.Drawing.Point(102, 36);
+            this.checkBoxPointsLeague.Name = "checkBoxPointsLeague";
+            this.checkBoxPointsLeague.Size = new System.Drawing.Size(126, 17);
+            this.checkBoxPointsLeague.TabIndex = 3;
+            this.checkBoxPointsLeague.Text = "Points-based League";
+            this.checkBoxPointsLeague.UseVisualStyleBackColor = true;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(13, 103);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(35, 13);
+            this.labelStatus.TabIndex = 4;
+            this.labelStatus.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(330, 133);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.checkBoxPointsLeague);
+            this.Controls.Add(this.textBoxLeagueNumber);
+            this.Controls.Add(this.labelLeagueNumber);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Yahoo Fantasy NHL Downloader v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelLeagueNumber;
+        private System.Windows.Forms.TextBox textBoxLeagueNumber;
+        private System.Windows.Forms.CheckBox checkBoxPointsLeague;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -26,97 +27,174 @@ namespace NhlDownload
             Opponent,
             Owner,
             GamesPlayed,
+            FanPoints,
             PreSeasonRanking,
             CurrentRanking,
             OwnedPercentage,
             TimeOnIce,
-            Goals,
-            Assists,
-            Points,
-            PlusMinus,
-            PenaltyMinutes,
-            PowerPlayPoints,
-            ShortHandedPoints,
-            ShotsOnGoal,
-            FaceoffWins,
-            Hits,
-            Blocks
+            Stat1,
+            Stat2,
+            Stat3,
+            Stat4,
+            Stat5,
+            Stat6,
+            Stat7,
+            Stat8,
+            Stat9,
+            Stat10
         }
+
+        string[] Positions = new string[] { "C", "R", "L", "D" };
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int pages=0; pages < 26; pages++) //26
+            int leagueNumber = 0;
+            labelStatus.Text = "Downloading ...";
+            labelStatus.Visible = true;
+            button1.Enabled = false;
+            if (int.TryParse(textBoxLeagueNumber.Text, out leagueNumber))
             {
-                String baseUrl = "https://hockey.fantasysports.yahoo.com/hockey/2561/players?status=ALL&pos=P&cut_type=33&stat1=S_S_2017&myteam=0&sort=AR&sdir=1";
-                String pageUrl;
-                if (pages ==0)
-                {
-                    pageUrl = baseUrl; 
-                } else
-                {
-                    pageUrl = baseUrl + "&count=" + (pages * 25);
-                }
+                if (leagueNumber <= 0) return;
+            }
+            else
+            {
+                return;
+            }
 
-                HtmlAgilityPack.HtmlWeb getHtmlWeb = new HtmlAgilityPack.HtmlWeb();
-                HtmlAgilityPack.HtmlDocument document = getHtmlWeb.Load(pageUrl);
-
-                // Page is loaded, go through the stats for each player
-                var players = document.DocumentNode.SelectNodes("//div[@id='players-table']//div[@class='players']//table//tbody//tr");
-                //Debug.WriteLine("Players found =" + players.Count());
-                foreach (HtmlAgilityPack.HtmlNode player in players)
+            try
+            {
+                using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter("NHL" + leagueNumber.ToString() + ".csv"))
                 {
-                    var items = player.Descendants("td");
-                    int playerTableColumnCounter = 0;
-                    foreach (HtmlAgilityPack.HtmlNode item in items)
+                    for (int pages = 0; pages < 30; pages++) //30 * 25 players per page = top 625 players
                     {
-                        switch ((PlayerTableColumnInfo)playerTableColumnCounter)
+                        //String baseUrl = "https://hockey.fantasysports.yahoo.com/hockey/" + leagueNumber.ToString() + "/players?status=ALL&pos=P&cut_type=33&stat1=S_S_2017&myteam=0&sort=AR&sdir=1";
+                        String baseUrl = "https://hockey.fantasysports.yahoo.com/hockey/" + leagueNumber.ToString() + "/players?&sort=AR&sdir=1&status=ALL&pos=P&stat1=S_S_2017&jsenabled=1";
+                        String pageUrl;
+                        if (pages == 0)
                         {
-                            case PlayerTableColumnInfo.WatchList:
-                                break;
-
-                            case PlayerTableColumnInfo.Player:
-                                string playerName = item.SelectSingleNode(".//a[@class='Nowrap name F-link']").InnerText;
-                                string teamPosition = item.SelectSingleNode(".//span[@class='Fz-xxs']").InnerText;
-                                string[] tm = teamPosition.Split('-');
-                                string team = tm[0].Trim();
-                                string position = tm[1].Replace(",", "-").Trim();
-                                Debug.Write(playerName + "," + team + "," + position);
-                                break;
-
-                            //case PlayerTableColumnInfo.AddDropTrade:
-                            //    string adtRaw = item.SelectSingleNode(".//a").Attributes["title"].Value;
-                            //    string[] adt = adtRaw.Split(' ');
-                            //    Debug.Write("," + adt[0].Trim());
-                            //    break;
-
-                            case PlayerTableColumnInfo.Opponent:
-                            case PlayerTableColumnInfo.Owner:
-                            case PlayerTableColumnInfo.GamesPlayed:
-                            case PlayerTableColumnInfo.PreSeasonRanking:
-                            case PlayerTableColumnInfo.CurrentRanking:
-                            case PlayerTableColumnInfo.OwnedPercentage:
-                            case PlayerTableColumnInfo.TimeOnIce:
-                            case PlayerTableColumnInfo.Goals:
-                            case PlayerTableColumnInfo.Assists:
-                            case PlayerTableColumnInfo.Points:
-                            case PlayerTableColumnInfo.PlusMinus:
-                            case PlayerTableColumnInfo.PenaltyMinutes:
-                            case PlayerTableColumnInfo.PowerPlayPoints:
-                            case PlayerTableColumnInfo.ShortHandedPoints:
-                            case PlayerTableColumnInfo.ShotsOnGoal:
-                            case PlayerTableColumnInfo.FaceoffWins:
-                            case PlayerTableColumnInfo.Hits:
-                            case PlayerTableColumnInfo.Blocks:
-                                string value = item.SelectSingleNode(".//div").InnerText;
-                                Debug.Write("," + value);
-                                break;
-
+                            pageUrl = baseUrl;
                         }
-                        playerTableColumnCounter++;
+                        else
+                        {
+                            pageUrl = baseUrl + "&count=" + (pages * 25);
+                        }
+
+                        //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(pageUrl);
+                        //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                        HtmlAgilityPack.HtmlWeb getHtmlWeb = new HtmlAgilityPack.HtmlWeb();
+                        HtmlAgilityPack.HtmlDocument document = getHtmlWeb.Load(pageUrl);
+
+                        // Page is loaded, go through the stats for each player
+                        var players = document.DocumentNode.SelectNodes("//div[@id='players-table']//div[@class='players']//table//tbody//tr");
+                        //Debug.WriteLine("Players found =" + players.Count());
+
+                        foreach (HtmlAgilityPack.HtmlNode player in players)
+                        {
+                            var items = player.Descendants("td");
+                            int playerTableColumnCounter = 0;
+                            string value;
+                            foreach (HtmlAgilityPack.HtmlNode item in items)
+                            {
+                                switch ((PlayerTableColumnInfo)playerTableColumnCounter)
+                                {
+                                    case PlayerTableColumnInfo.WatchList:
+                                    case PlayerTableColumnInfo.AddDropTrade:
+                                    case PlayerTableColumnInfo.Opponent:
+                                        //    string adtRaw = item.SelectSingleNode(".//a").Attributes["title"].Value;
+                                        //    string[] adt = adtRaw.Split(' ');
+                                        //    Debug.Write("," + adt[0].Trim());
+                                        break;
+
+                                    case PlayerTableColumnInfo.Player:
+                                        string playerName = item.SelectSingleNode(".//a[@class='Nowrap name F-link']").InnerText;
+                                        string teamPosition = item.SelectSingleNode(".//span[@class='Fz-xxs']").InnerText;
+                                        string[] tm = teamPosition.Split('-');
+                                        string team = tm[0].Trim();
+                                        file.Write(playerName + "," + team);
+                                        string position = tm[1].Replace(",", "").Trim();
+                                        position = position.Replace("W", "");
+                                        file.Write("," + position);
+                                        for (int p = 0; p < Positions.Length; p++)
+                                        {
+                                            if (position.Contains(Positions[p]))
+                                                file.Write("," + Positions[p]);
+                                            else
+                                                file.Write(",");
+                                        }
+                                        break;
+
+                                    case PlayerTableColumnInfo.TimeOnIce:
+                                        value = item.SelectSingleNode(".//div").InnerText;
+                                        if (checkBoxPointsLeague.Checked)
+                                        {
+                                            if (value.Equals("-"))
+                                                file.Write(",00:00:00");
+                                            else
+                                                file.Write(",00:" + value);
+                                        }
+                                        else
+                                        {
+                                            if (value.Equals("-"))
+                                                file.Write(",0");
+                                            else
+                                                file.Write("," + value);
+                                            break;
+                                        }
+                                        break;
+
+                                    case PlayerTableColumnInfo.OwnedPercentage:
+                                        value = item.SelectSingleNode(".//div").InnerText;
+                                        if (!checkBoxPointsLeague.Checked)
+                                        {
+                                            if (value.Equals("-"))
+                                                file.Write(",00:00:00");
+                                            else
+                                                file.Write(",00:" + value);
+                                        }
+                                        else
+                                        {
+                                            if (value.Equals("-"))
+                                                file.Write(",0");
+                                            else
+                                                file.Write("," + value);
+                                            break;
+                                        }
+                                        break;
+
+                                    default:
+                                        value = item.SelectSingleNode(".//div").InnerText;
+                                        if (value.Equals("-"))
+                                            file.Write(",0");
+                                        else
+                                            file.Write("," + value);
+                                        break;
+                                }
+                                playerTableColumnCounter++;
+                            }
+                            file.WriteLine("");
+                        }
                     }
-                    Debug.WriteLine("");
+                    labelStatus.Text = "Downloaded to " + "NHL" + leagueNumber.ToString() + ".csv !";
+                    labelStatus.Visible = true;
                 }
             }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception=" + ex.Message);
+                labelStatus.Text = "Failed download!";
+                labelStatus.Visible = true;
+            }
+            finally
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelStatus.Visible = false;
         }
     }
 }
